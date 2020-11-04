31847d3a498badb5a5cdcee8b42fe03c429a4173
eeong <discography8@gmail.com>
2020-11-04 13:23:33 +0900

    201104 list

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const books = [
+	{id:1, title: '별주부전', writer: '작자미상', content:"불법 장기매매"},
+	{id:2, title: '심청전', writer: '작자미상', content:"인신매매, 사기"},
+	{id:3, title: '춘향전', writer: '작자미상', content:"공권력 남용"},
+];
+
 /** 서버구동 *******************************/
 app.listen(3000, () => {
 	console.log("=====================");
@@ -13,11 +19,35 @@ app.listen(3000, () => {
 /** 설정 *******************************/
 app.set('view engine', 'pug');
 app.set('views','./views');
-
+app.locals.pretty = true;
 
 /** 라우터 *******************************/
 app.use('/', express.static(path.join(__dirname, './public')));
 
-app.get('/sample',(req, res) => {
-	res.render('sample');
+app.get('/list',(req, res) => {
+	const pug = {
+		title : {
+			head : '도서 리스트',
+			body : '도서 목록',
+			small : '소설'
+		},
+		lists: [
+			...books
+		]
+	};
+	res.render('book/list', pug);
+}); 
+
+app.get('/book/:id',(req, res) => {
+	const pug = {
+		title : {
+			head : '도서 리스트',
+			body : '도서 목록',
+			small : '소설'
+		},
+		lists: [
+			...books
+		]
+	};
+	res.render('book/view', pug);
 }); 
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -6,8 +6,24 @@ var express = require('express');
 var app = express();
 
 var path = require('path');
-/** 서버구동 *******************************/
 
+var books = [{
+  id: 1,
+  title: '별주부전',
+  writer: '작자미상',
+  content: "불법 장기매매"
+}, {
+  id: 2,
+  title: '심청전',
+  writer: '작자미상',
+  content: "인신매매, 사기"
+}, {
+  id: 3,
+  title: '춘향전',
+  writer: '작자미상',
+  content: "공권력 남용"
+}];
+/** 서버구동 *******************************/
 
 app.listen(3000, function () {
   console.log("=====================");
@@ -18,9 +34,29 @@ app.listen(3000, function () {
 
 app.set('view engine', 'pug');
 app.set('views', './views');
+app.locals.pretty = true;
 /** 라우터 *******************************/
 
 app.use('/', express["static"](path.join(__dirname, './public')));
-app.get('/sample', function (req, res) {
-  res.render('sample');
+app.get('/list', function (req, res) {
+  var pug = {
+    title: {
+      head: '도서 리스트',
+      body: '도서 목록',
+      small: '소설'
+    },
+    lists: [].concat(books)
+  };
+  res.render('book/list', pug);
+});
+app.get('/book/:id', function (req, res) {
+  var pug = {
+    title: {
+      head: '도서 리스트',
+      body: '도서 목록',
+      small: '소설'
+    },
+    lists: [].concat(books)
+  };
+  res.render('book/view', pug);
 });
